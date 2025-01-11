900adbadf34ee93fe1db05c9619935421bc53574
gauravtripathii <gauravtripathii7979@gmail.com>
2025-01-11 14:05:23 +0530

    edit button

--- src/components/TasksListing.tsx
+++ src/components/TasksListing.tsx
@@ -32,13 +32,19 @@ const TasksListing = ({ tasks, addToTasks }: any) => {
         <div className='flex flex-col gap-3'>
             {
                 tasks.map((task: taskType, index: number) => (
-                    <div key={index} className={`individual-task-container w-full h-20 rounded-xl flex items-center ${task.status as unknown as string === "Completed" ? "dark-green-bg" : task.status as unknown as string === "In Progress" ? "medium-yellow-bg" : "light-red-bg"}`}>
-                        <div className={`task-icon-container p-3 w-20`}>
-                            <Image src={`/assets/task icons/${task.icon}.gif`} alt='computer' height={900} width={900} className="w-full h-full rounded-lg" />
+                    <div key={index} onClick={() => console.log("double clicked!")} className={`individual-task-container w-full h-20 rounded-xl flex items-center justify-between ${task.status as unknown as string === "Completed" ? "dark-green-bg" : task.status as unknown as string === "In Progress" ? "medium-yellow-bg" : "light-red-bg"}`}>
+                        <div className="flex items-center">
+                            <div className={`task-icon-container p-3 w-20`}>
+                                <Image src={`/assets/task icons/${task.icon}.gif`} alt='computer' height={900} width={900} className="w-full h-full rounded-lg" />
+                            </div>
+
+                            <div className="text-[17px] font-medium capitalize">{task.task_name}</div>
                         </div>
 
-                        <div className="text-[20px] font-medium capitalize">{task.task_name}</div>
-                        
+                        <div className="task-icon-container p-7 w-20">
+                            <Image src={`/assets/Edit_duotone.svg`} alt='computer' height={900} width={900} className="w-full h-full rounded-lg" />
+                        </div>
+
                     </div>
                 ))
             }
