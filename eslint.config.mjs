dc74cd4a8d59d30a2da5b3d9c7b3816db62cf621
gauravtripathii <gauravtripathii7979@gmail.com>
2025-01-14 19:42:56 +0530

    eslint.config.mjs changed to facilitate the deployment on vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["next.config.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Disable rule for this file
+    },
+  },
 ];
 
 export default eslintConfig;
